f7927812cb00e8a35ab7a7061a81646000492129
ravi_ <ravi_@RK-SAMSUNG-LT>
2023-05-16 14:30:56 -0400

    Add project files.

---
+++ RavikScriptTest_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RavikScriptTest_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("RavikScriptTest_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("BA881660-EE7A-4A09-A172-894734895B5E")]
+[assembly: AssemblyVersion("1.0.0.0")]
